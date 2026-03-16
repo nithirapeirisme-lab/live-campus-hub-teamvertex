5f8b94dee0e44c40faa51fdce1b131d63efd9d98
sasaara <pamudisasaara@gmail.com>
2026-03-16 12:39:04 +0530

    Implement CORS configuration for mobile integration

---
+++ campus-hub/src/main/java/com/campushub/campus_hub/Config/CORSConfig.java
@@ -0,0 +1,23 @@
+package com.campushub.campus_hub.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CORSConfig {
+    @Bean
+    public WebMvcConfigurer configurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
